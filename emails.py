6a4fa43e3b4fda393f27aa84e56dd626487c2a84
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-01-27 10:43:57 +0100

    ajout de la fonction pd

--- emails.py
+++ emails.py
@@ -27,16 +27,28 @@ def get_movies():
 
     return movie
 
+def get_pd():
+    try:
+        pds = open('liste_des_pd.txt', 'r')
+        pds = pds.read()
+    except FileNotFoundError as err:
+        print(err)
+
+    return pd
+
 def main():
     emails = get_emails()
     print(emails)
 
     movie = get_movies()
     print(movie)
+    
+    pd = get_pd()
+    print(pd)
 
     forcast = weather.get_weather_forcast()
     print(forcast)
 
-    mailer.send_emails(emails, movie, forcast)
+    mailer.send_emails(emails, movie, pd, forcast)
 
 main()
